2325308aa69fffdfdb4f58698eb9f5b5ffd39924
ChromeMatic <armanibrwn@gmail.com>
2024-12-05 12:13:57 -0500

    vision & mission statement added

--- pages/aboutUs.vue
+++ pages/aboutUs.vue
@@ -6,9 +6,11 @@ definePageMeta({ layout: 'default'})
 <template>
   <section
       v-motion="animation"
-      class="w-full flex flex-col space-y-4 flex-grow rounded-md items-center font-GeistMono overflow-hidden "
+      class="w-full flex flex-col justify-center space-y-4 flex-grow rounded-md items-center font-GeistMono overflow-hidden "
       :class="useTheme() ? 'bg-Dark':'bg-white'"
   >
-
+          <h1 class="text-4xl">
+            About Us
+          </h1>
   </section>
 </template>
